e07c702e742d6a0c53a352e2af2fc7948111d24a
Petr Kočí <petr.koci@samizdat.cz>
2022-05-06 11:59:22 +0200

    ekonomické dopady

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function middleware(req, ev) {
   const { pathname, origin } = req.nextUrl;
   if (pathname == "/") {
-    return NextResponse.redirect(`${origin}/dopad`);
+    return NextResponse.redirect(`${origin}/ekonomicke-dopady`);
   }
   return NextResponse.next();
 }
