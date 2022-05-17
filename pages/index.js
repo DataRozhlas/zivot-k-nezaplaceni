c1025d21ba1734ce37cb00c896705eedd62efed7
Petr Kočí <petr.koci@samizdat.cz>
2022-05-17 16:05:14 +0200

    router push

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Layout from "../components/layout";
 import { useEffect } from "react";
-//import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   // Always do navigations after the first render
-  //   router.push("/ekonomicke-dopady", undefined, { shallow: true });
-  // }, []);
+  useEffect(() => {
+    // Always do navigations after the first render
+    router.push("/ekonomicke-dopady", undefined, { shallow: true });
+  }, []);
 
   return <Layout></Layout>;
 }
