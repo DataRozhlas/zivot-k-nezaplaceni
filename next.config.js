cebe8c5335e376a997f5e67dd092267639cba422
Petr Kočí <petr.koci@samizdat.cz>
2022-05-18 17:26:51 +0200

    export dir zivot -> out

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  basePath: "/zivot-k-nezaplaceni/zivot", //before static deployment, change this to "/zivot"
+  basePath: "/zivot-k-nezaplaceni/out", //before static deployment, change this to "/zivot"
   images: {
     loader: "custom",
   },
