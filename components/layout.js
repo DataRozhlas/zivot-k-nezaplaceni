359b64e09b0474f71dd5e1ae29f670a7c7db6021
Petr Kočí <petr.koci@samizdat.cz>
2022-08-22 13:02:15 +0200

    change default page

--- components/layout.js
+++ components/layout.js
@@ -51,7 +51,7 @@ export default function Layout(props) {
 
   useEffect(() => {
     if (router.pathname === "/") {
-      router.push("/posilovaci");
+      router.push("/vydaje-castky");
     }
   }, []);
 
