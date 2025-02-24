9ad8dc0cf93f4bf72b2327d17404dbf44e982c4c
Petr Kočí <petr.koci@samizdat.cz>
2025-02-24 13:31:27 +0100

    update logo

--- pages/projekt.js
+++ pages/projekt.js
@@ -29,10 +29,10 @@ export default function About({ menu }) {
       </p>
       <p>
         Právě to zjišťuje výzkum{" "}
-        <strong>Česko 2022: Život k nezaplacení</strong> od jara 2022 do současnosti. 
-          V jeho průběhu postupně ukazujeme i další témata. Například
-        jak Češi vnímají integraci lidí z Ukrajiny v různých oblastech života nebo
-        jak na ně ekonomická situace a nedaleká válka dopadá psychicky.
+        <strong>Česko 2022: Život k nezaplacení</strong> od jara 2022 do
+        současnosti. V jeho průběhu postupně ukazujeme i další témata. Například
+        jak Češi vnímají integraci lidí z Ukrajiny v různých oblastech života
+        nebo jak na ně ekonomická situace a nedaleká válka dopadá psychicky.
       </p>
       <p>
         Každý měsíc počínaje dubnem 2022 oslovujeme vzorek stejných respondentů
@@ -87,13 +87,15 @@ export default function About({ menu }) {
       </p>
       <p>
         Další zdroje na dotazování zbytku výzkumu a tvorbu předchozích výstupů
-        pocházejí zejména z programu Strategie AV21 Akademie věd ČR a Národního institutu SYRI.
-          Ten se podílí na financování sběru dat od podzimu 2022 do jara 2025.
-        Národní institut pro výzkum socioekonomických dopadů nemocí a systémových rizik (SYRI) 
-          sdružuje přibližně 150 vědců z Masarykovy univerzity, Univerzity Karlovy a Akademie věd ČR. Zkoumá rizika, jako je pandemie covid-19, inflace, 
-            dezinformace nebo krize důvěry v instituce, a hledá cesty, jak je zmírňovat.
-        V minulosti bylo panelové šetření financováno i ze zdrojů Technologické agentury ČR,
-        Max Planck Institute a dalších donorských zdrojů.
+        pocházejí zejména z programu Strategie AV21 Akademie věd ČR a Národního
+        institutu SYRI. Ten se podílí na financování sběru dat od podzimu 2022
+        do jara 2025. Národní institut pro výzkum socioekonomických dopadů
+        nemocí a systémových rizik (SYRI) sdružuje přibližně 150 vědců z
+        Masarykovy univerzity, Univerzity Karlovy a Akademie věd ČR. Zkoumá
+        rizika, jako je pandemie covid-19, inflace, dezinformace nebo krize
+        důvěry v instituce, a hledá cesty, jak je zmírňovat. V minulosti bylo
+        panelové šetření financováno i ze zdrojů Technologické agentury ČR, Max
+        Planck Institute a dalších donorských zdrojů.
       </p>
       <h2>Metodické poznámky</h2>
       <p>
@@ -282,10 +284,10 @@ export default function About({ menu }) {
           </div>
         </Link>
         <Link href="https://www.paqresearch.cz/">
-          <div className={styles.logo}>
+          <div className={styles.logo} style={{ marginTop: "-5px" }}>
             <Image
               src="../logo/paq.svg"
-              height={40}
+              height={80}
               width={120}
               alt="logo"
               unoptimized={true}
